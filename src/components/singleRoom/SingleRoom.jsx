e8bba7fad3c89bfed8694bad6908e8869e3b1a62
Alex <belohvostov.belohvostov@gmail.com>
2022-01-19 20:41:58 +0300

    searching about slug

---
+++ src/components/singleRoom/SingleRoom.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Hero } from '../Hero'
+import { Banner } from '../Banner'
+import { Link } from 'react-router-dom'
+
+export const SingleRoom = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
